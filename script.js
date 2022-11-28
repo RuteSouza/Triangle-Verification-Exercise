055d4987df67c59867dbe57ebfa06eda0418dbb5
Rute Souza <75092581+RuteSouza@users.noreply.github.com>
2022-11-28 23:06:25 +0000

    JS file changed

--- script.js
+++ script.js
@@ -1,30 +1,51 @@
-function Triangle(lado1,lado2, lado3){
-  this.lado1 = lado1;
-  this.lado2 = lado2;
-  this.lado3 = lado3;
-  this.getArea = function(){
-  var p = this.getPerimeter()/2;
-    return Math.sqrt (p*(p-lado1)* (p-lado2)*(p-lado3));
+function calculateTriangleProperties() {
+  var s1 = parseFloat(document.getElementById('lado1').value);
+  var s2 = parseFloat(document.getElementById('lado2').value);
+  var s3 = parseFloat(document.getElementById('lado3').value);
+
+  if (isNaN(s1) || isNaN(s2) || isNaN(s3)) {
+    document.getElementById('data').innerHTML = 'Insira apenas números';
+    return;
   }
-  this.getPerimeter = function(){
-     return lado1 + lado2 + lado3;
+
+  if (s1 <= 0 || s2 <= 0 || s3 <= 0) {
+    document.getElementById('data').innerHTML =
+      'Números não positivos não são válidos';
+    return;
   }
-  this.toString = function(){
-    return "the triangle with sides = "+ this.lado1 + this.lado2 + this.lado3 + "Area = " + this.getArea () + "Add Perimeter=" + this.getPerimeter();
+
+  if (!isValidTriangle(s1, s2, s3)) {
+    document.getElementById('data').innerHTML =
+      'Os valores informados não podem formar um triâgulo. Tente novamente!';
+    return;
   }
-  function calculate() {
-    var s1 = parseFloat(document.getElementById ('lado1').value);
-    var s2 = parseFloat(document.getElementById ('lado2').value);
-    var s3 = parseFloat(document.getElementById ('lado3').value);
-    if (isNaN(s1)|| isNaN(s2) || isNaN(s3)){
-      document.getElementById('data').innerHTML = "Insira apenas números";
-      return;
-    }
-    else if (s1<=0 || s2<=0 || s3<=0){
-      document.getElementById ('data').innerHTML = "Números negativos não são válidos";
-      return;
-      }
-    var myTriangle = new Triangle(s1,s2,s3);
-    document.getElementById('data').innerHTML = myTriangle.toString();
-  }
+
+  document.getElementById('data').innerHTML = createOutput(s1, s2, s3);
+}
+
+function isValidTriangle(lado1, lado2, lado3) {
+  if (
+    lado1 + lado2 <= lado3 ||
+    lado1 + lado3 <= lado2 ||
+    lado2 + lado3 <= lado1
+  )
+    return false;
+  else return true;
+}
+
+function getTrianglePerimeter(lado1, lado2, lado3) {
+  return lado1 + lado2 + lado3;
+}
+
+function getTriangleArea(lado1, lado2, lado3) {
+  var semiPermineter = getTrianglePerimeter(lado1, lado2, lado3) / 2;
+  return Math.sqrt(
+    semiPermineter *
+      (semiPermineter - lado1) *(semiPermineter - lado2) *(semiPermineter - lado3)
+  );
+}
+function createOutput(lado1, lado2, lado3) {
+  return `Triângulos com lados ${lado1}, ${lado2}, ${lado3} <br />
+  tem uma área de = ${getTriangleArea(lado1, lado2, lado3)} <br/>
+  e perimetro de ${getTrianglePerimeter(lado1, lado2, lado3)}`;
 }
